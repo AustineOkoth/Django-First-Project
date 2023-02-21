eaf8466ba665935ec935d01918f92b14a9634a81
Austine Okoth Okongo <67043517+Austine4321@users.noreply.github.com>
2023-02-21 14:58:34 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- austineblog/views.py
+++ austineblog/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
 # Create your views here.
+
+#Post.objects.get(pk=pk)
+
+
 def post_list(request):
      posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
      return render(request, 'austineblog/post_list.html', {'posts' : posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'austineblog/post_detail.html', {'post': post})
